d6861b0433d13d935d713d5aab14e266a9f17d55
alux <ahmadabd13741112@gmail.com>
2022-01-05 07:44:28 +0330

    File Upload

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"web_service/database"
 	"web_service/product"
+	"web_service/receipt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -14,6 +15,7 @@ const basePath = "/api"
 
 func main() {
 	database.SetupDatabase()
+	receipt.SetupRoutes(basePath)
 	product.SetupRoutes(basePath)
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
